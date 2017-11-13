5ee00fa28059d67508c7669d0fb9543c77ef1bb1
jmgoyesc <jmgoyesc@gmail.com>
2017-11-13 13:23:55 +0100

    Create model for method reference error

---
+++ src/main/java/com/personal/sample/test/other/AbstractClass.java
@@ -0,0 +1,11 @@
+package com.personal.sample.test.other;
+
+abstract class AbstractClass {
+	private String id;
+	private Long key;
+
+	public String getId() {return id;}
+	public void setId(String id) {this.id = id;}
+	public Long getKey() {return key;}
+	public void setKey(Long key) {this.key = key;}
+}
---
+++ src/main/java/com/personal/sample/test/other/PublicClass.java
@@ -0,0 +1,8 @@
+package com.personal.sample.test.other;
+
+public class PublicClass extends AbstractClass {
+	@Override
+	public String toString() {
+		return "PublicClass{id="+getId()+", key="+getKey()+"}";
+	}
+}
